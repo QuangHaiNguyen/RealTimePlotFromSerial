05fb9ee406f24e115a1df69c1598905cbba8f343
Quang Hai Nguyen <hainguyen.eeit@gmail.com>
2019-01-03 11:14:29 +0100

    add 2 threads into project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RealTimePlotFromSerial
+# blah blah
--- RealTimePlotFromSerial.py
+++ RealTimePlotFromSerial.py
@@ -1,5 +1,6 @@
-print("hello world")
 
+import threading
+import time
 
 def help_cmd_handle():
     print('help function')
@@ -8,15 +9,43 @@ def help_cmd_handle():
 cmd_dict = {'help': help_cmd_handle}
 
 
+class CLIThread(threading.Thread):
+    def __init__(self, thread_id, thread_name):
+        threading.Thread.__init__(self)
+        self.thread_id = thread_id
+        self.thread_name = thread_name
+
+    def run(self):
+        while 1:
+            usr_input = input("> ")
+            user_input_split = usr_input.lower().split(' ')  # make sure the command is in lowercase
+            if user_input_split[0] == 'quit':
+                break
+            for cmd, function in cmd_dict.items():
+                if user_input_split[0] == cmd:
+                    function()
+        print('quit main function')
+
+
+class SerialThread(threading.Thread):
+    def __init__(self, thread_id, thread_name):
+        threading.Thread.__init__(self)
+        self.thread_id = thread_id
+        self.thread_name = thread_name
+
+    def run(self):
+        while 1:
+            print(self.thread_name)
+            time.sleep(5)
+
+
 def main():
-    usr_input = ""
-    while usr_input.lower() != 'quit':
-        usr_input = input("> ")
-        user_input_split = usr_input.lower().split(' ')  # make sure the command is in lowercase
-        for cmd, function in cmd_dict.items():
-            if user_input_split[0] == cmd:
-                function()
-    print('quit main function')
+    cli_thread = CLIThread(1, "CLI_Thread")
+    serial_thread = SerialThread(2, "SerialThread")
+    serial_thread.start()
+    cli_thread.start()
+    cli_thread.join()
+    serial_thread.join()
 
 
 main()
